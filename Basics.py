fee992cc15b1ff84905c7e3d661cdfac295f8198
Muhammad Haris <raoh0163@gmail.com>
2025-09-15 07:22:30 +0000

    Intro , Syntax , Comments

---
+++ Basics.py
@@ -0,0 +1,22 @@
+print("Hello, World!")
+
+if 5 > 2:
+  print("Five is greater than two!") # Indentation is important in Python means Spaces or Tabs
+  print("Five is still greater than two!")# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error
+            
+x = 5 #variables are created when you assign a value to it
+y = "Hello, World!"
+print(x)
+print(y)
+
+"""
+x = 5 #variables are created when you assign a value to it
+y = "Hello, World!"
+print(x)
+print(y)
+
+"""
+#This is a comment method
+
+            
+
